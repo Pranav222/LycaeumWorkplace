33c2093902463d2cdf071715a8c2ca8afcff921f
pranav <pranavkv222@gmail.com>
2017-09-18 08:36:26 +0000

    biggest of two number

---
+++ ex5.py
@@ -0,0 +1,8 @@
+""" Write a function called biggest which takes two numerical arguments
+a and b and returns the larger of the two."""
+
+def biggest(a,b):
+    if a > b:
+        return a
+    else:
+        return b
