4643e41226e28cf754a67faddda4d03efa8bc0ba
pranav <pranavkv222@gmail.com>
2017-09-20 10:21:58 +0000

    panagram

--- ex9.py
+++ ex9.py
@@ -1,18 +1,13 @@
-"""Write a function called fizzbizz which takes an integer argument n.
-It should print numbers from 1 to n with the following conditions.
- if the number is divisible by 3, it should print fizz
- if the number is divisible by 5, it should print bizz
- if the number is divisible by 15, it should print fizzbizz
- Otherwise, just print the number."""
+"""Panagram A panagram is a sentence which contains all letters of the
+alphabet.
+Write a function called panagram which takes a string s as input. It
+should return True if s is a panagram and False otherwise.
+"""
 
-def fizzbizz(n):
-    a = range(1,n+1)
-    for i in a:
-        if i % 15 ==0:
-            print ("fizzbizz")
-        elif i % 5 ==0:
-            print ("bizz")
-        elif i % 3 ==0:
-            print("fizz")
-        else:
-            print(i)
+import string
+def panagram(s): # applicable only for lowercase letters
+    az=string.ascii_lowercase 
+    for i in az:
+        if i not in s:
+            return False
+    return True
